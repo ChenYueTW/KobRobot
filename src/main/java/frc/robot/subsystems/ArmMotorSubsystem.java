68362a041ca617ad30207dc02824be5ef84cd1f0
ChenYueTW <eagleh2005@gmail.com>
2023-09-25 20:02:49 +0800

    Upload

--- src/main/java/frc/robot/subsystems/ArmMotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmMotorSubsystem.java
@@ -13,32 +13,32 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmMotorSubsystem extends SubsystemBase{
 
-    private CANSparkMax ArmMotor1;
+    private CANSparkMax ArmMotor;
 
     private double Arm_speed;
 
     public ArmMotorSubsystem() {
 
-        ArmMotor1 = new CANSparkMax(robotMap.DriverPort.Arm_Port.Arm1MotorPort, MotorType.kBrushless);
+        ArmMotor = new CANSparkMax(robotMap.DriverPort.Arm_Port.Arm1MotorPort, MotorType.kBrushless);
 
-        ArmMotor1.setSmartCurrentLimit(30);
+        ArmMotor.setSmartCurrentLimit(30);
 
         // kBrake & kCoast
-        ArmMotor1.setIdleMode(IdleMode.kBrake);
+        ArmMotor.setIdleMode(IdleMode.kBrake);
 
-        ArmMotor1.setInverted(false);
+        ArmMotor.setInverted(false);
     }
 
     public void setDesiredState(Double ArmSpeed) {
 
         this.Arm_speed = ArmSpeed * Constants.DriveConstants.ArmSpeed;
-        ArmMotor1.set(Arm_speed);
+        ArmMotor.set(Arm_speed);
 
         SmartDashboard.putNumber("Arm-Speed: ", Arm_speed);
         
     }
 
     public void stop() {
-        ArmMotor1.set(0);
+        ArmMotor.set(0);
     }
 }
