ba4e0db79c2df27b3d281c9288709f4abd090d5b
ChenYueTW <eagleh2005@gmail.com>
2023-10-22 21:43:08 +0800

    Upload

--- src/main/java/frc/robot/GamepadJoystick.java
+++ src/main/java/frc/robot/GamepadJoystick.java
@@ -7,17 +7,8 @@ public class GamepadJoystick extends XboxController {
     // COM ID
     public static final int DRIVER_CONTROLLER_PORT = 0;
     public static final int ARM_CONTORLLER_PORT = 1;
-    private static final double DEADBAND = 0.05;
 
     public GamepadJoystick(final int port) {
         super(port);
-    }
-
-    public double getRobotDriveSpeed() {
-        return MathUtil.applyDeadband(-this.getLeftY(), DEADBAND);
-    }
-
-    public double getRobotRotateSpeed() {
-        return MathUtil.applyDeadband(-this.getRightX(), DEADBAND) * Constants.Drive.TURN_SPEED;
     }
 }
